57237b8d975091143a75304f3242ca84ea710576
gliech <gliech@users.noreply.github.com>
2018-07-13 09:48:15 +0200

    Made the output from the install_python provisioner more informative.

--- vagrant/install-python.sh
+++ vagrant/install-python.sh
@@ -1,15 +1,19 @@
-if ! which python
+if ! which python >& /dev/null
 then
-    if which yum
+    echo 'Installing Python.'
+    if which yum >& /dev/null
     then
         yum -yq install python || exit 2
-    elif which apt
+    elif which apt >& /dev/null
     then
         apt -yqq install python || exit 2
-    elif which pacman
+    elif which pacman >& /dev/null
     then
         pacman --noconfirm --noprogressbar --needed -S python || exit 2
     else
+        echo 'No supported package manager found.' 1>&2
         exit 3
     fi
+else
+    echo 'Python already installed.'
 fi
