d0fd3a66276d144f7c9e00e419fcc8538a15f22e
gliech <gliech@users.noreply.github.com>
2018-10-22 15:38:00 +0200

    Added an installation routine for chattr into a script that uses it

--- vagrant/dhcp/ignore-vagrant-dns.sh
+++ vagrant/dhcp/ignore-vagrant-dns.sh
@@ -1,3 +1,23 @@
+if ! which chattr >& /dev/null
+then
+    echo 'Installing Filesystem Utilities.'
+    if which yum >& /dev/null
+    then
+        yum --assumeyes --quiet install e2fsprogs || exit 2
+    elif which apt-get >& /dev/null
+    then
+        apt-get --yes --quiet=2 --no-install-recommend install e2fsprogs || exit 2
+    elif which pacman >& /dev/null
+    then
+        pacman --noconfirm --noprogressbar --needed -S e2fsprogs || exit 2
+    else
+        echo 'No supported package manager found.' 1>&2
+        exit 3
+    fi
+else
+    echo 'Filesystem Utilities already installed.'
+fi
+
 if [[ -L /etc/resolv.conf ]]
 then
     rm /etc/resolv.conf
