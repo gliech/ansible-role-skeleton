3eb51d1d3fb3700cd9ec2b7fe4da64f93b28c563
gliech <gliech@users.noreply.github.com>
2020-04-26 12:41:27 +0200

    Added some options to install routines in vagrant shell scripts

--- vagrant/install-python.sh
+++ vagrant/install-python.sh
@@ -3,13 +3,13 @@ then
     echo 'Installing Python.'
     if which yum >& /dev/null
     then
-        yum --assumeyes --quiet install python || exit 2
+        yum --assumeyes --quiet --errorlevel=0 --nogpgcheck install python || exit 2
     elif which apt-get >& /dev/null
     then
         apt-get --yes --quiet=2 --no-install-recommend install python || exit 2
     elif which pacman >& /dev/null
     then
-        pacman --noconfirm --noprogressbar --needed -S python || exit 2
+        pacman --sync --noconfirm --noprogressbar --needed --quiet python || exit 2
     else
         echo 'No supported package manager found.' 1>&2
         exit 3
